495e68a7cd286144b360548214e883c9e6703488
tezike <tobeydaniels@gmail.com>
2022-03-24 08:53:19 +0100

    add port as env variable

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -4,7 +4,7 @@ import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
 
 fun main() {
-    embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
+    embeddedServer(Netty, port = System.getenv("PORT").toInt()) {
         module()
     }.start(wait = true)
 }
